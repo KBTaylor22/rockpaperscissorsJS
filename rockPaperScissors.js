8be98388cbb0d1eff7d8c8f941537166946cac47
your-user-name <your@email.com>
2022-10-25 19:24:08 -0400

    Test commit

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,14 +2,15 @@ function playRound(playerSelection, computerSelection) {
   // your code here to determine whether playerSelection or computerSelection wins!
   if (playerSelection === computerSelection) {
     console.log('Tie');
-  } else if (playerSelection === 'rock' && computerSelection === scissors) { 
-    console.log(`You chose ${playerSelection}. They chose ${computerSelection}. You win.`);
-  } else if (playerSelection === 'paper' && computerSelection === rock) { 
-    console.log(`You chose ${playerSelection}. They chose ${computerSelection}. You win.`);
-  } else if (playerSelection === 'scissors' && computerSelection === paper) { 
-    console.log(`You chose ${playerSelection}. They chose ${computerSelection}. You win.`);
-  } else {
-    console.log(`You chose ${playerSelection}. They chose ${computerSelection}. You loose.`);
+  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') { 
+       console.log(`You chose ${playerSelection}. They chose ${computerSelection}. You win.`);
+   } else if (playerSelection === 'paper' && computerSelection === 'rock') { 
+        console.log(`You chose ${playerSelection}. They chose ${computerSelection}. You win.`);
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') { 
+      console.log(`You chose ${playerSelection}. They chose ${computerSelection}. You win.`);
+   } else {
+  console.log(`You chose ${playerSelection}. They chose ${computerSelection}. You loose.`);
+   }
 }
 
 function getComputerChoice() {
@@ -19,10 +20,10 @@ function getComputerChoice() {
   case 1:
       return 'paper'
   case 2:
-      return 'rock'
+      return 'scissors'
   }
-}
+} 
 
-
+let playerSelection = prompt('Rock, Paper, Scissors. Pick one:');
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
